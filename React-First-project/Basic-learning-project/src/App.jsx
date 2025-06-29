1d8d1529515b62ba9733c9fffead7874a01cf14b
Aayush <aayushchandak2@gmail.com>
2025-06-29 10:49:05 +0530

    add

--- React-First-project/Basic-learning-project/src/App.jsx
+++ React-First-project/Basic-learning-project/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <h1>Count : {count}</h1>
       <button
         onClick={() => {
           setCount((count) => count + 1);
         }}
       >
-        count {count}
+        count
       </button>
+
+    
     </>
   );
 }
---
+++ React-First-project/Basic-learning-project/src/bar.jsx
@@ -0,0 +1,5 @@
+function bar() {
+  return <hi>bar</hi>;
+}
+
+export default bar;
